4b643836cb32f26f601ad4341142d10bfd956e5c
nikhil singh <nikhilsingh526452@gmail.com>
2023-07-25 08:16:57 +0530

    small bug fixes

--- coursera/src/Signin.tsx
+++ coursera/src/Signin.tsx
@@ -1,102 +1,117 @@
 import * as React from 'react';
+import axios from 'axios';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from 'axios';
-import {  toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
-const baseurl='http://localhost:3000'
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
 
-export default function SignIn() {
-  const navigate = useNavigate();
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    //event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const token= await axios.post(baseurl+'/admin/signup', {
-      username: data.get('email'),
-      password: data.get('password')
-    });
-    if(token){
-      localStorage.setItem('token','Bearer '+token.data.token);
-      navigate('/')
-      toast.success('Login Successfull');
-    }
-    else{
-      toast.error('Login Failed')
-      console.log(token)
-    }
+
+export default function   In() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const navigate = useNavigate();  
+  const handleSubmit = (event:any) => {
+    event.preventDefault();
+    axios( {
+      method: 'post',
+      url:'http://localhost:3000/admin/login',
+      headers: { 'Content-Type': 'application/json',username,password},
+      data:{}
+    }).then((response)=>{
+      if(response.status==200)
+      {
+        localStorage.setItem('token','Bearer '+response.data.token);
+        console.log('user logged in sucessfully');
+        navigate('/');
+      }
+      else{
+        alert('Error');
+        console.error(response);
+      }
+    }).catch(e => {
+      console.log(e);
+  });
   };
+  const handleUsernameChange = (event:any) => {
+    setUsername(event.target.value);
+  }
+  const handlePasswordChange = (event:any) => {
+    setPassword(event.target.value);
+  }
 
   return (
-    <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
         <Box
           sx={{
+            p:3,
+            boxShadow:'1',
             marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
           }}
+          bgcolor={'white'}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: 'Blue' }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
+          <Typography component="h1" variant="h5" color={'black'}>
+            Sign In
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  onChange={handleUsernameChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={handlePasswordChange}
+                />
+              </Grid>
+
+            </Grid>
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={handleSubmit}
             >
               Sign In
             </Button>
-            <Grid container>
+            <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="/signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                  Not a user ? Sign up
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
+
       </Container>
-    </ThemeProvider>
+    
   );
 }
